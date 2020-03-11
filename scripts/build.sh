49305bb162baeea8af36b7b6852a3436ff7c63dd
Arlind HOXHA <ahoxha.01@gmail.com>
2020-03-11 10:26:49 +0100

    syntax correction

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+mvn clean package -q
+sudo rm /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
+chmod +x docker-compose
+sudo mv docker-compose /usr/local/bin
+
 docker build -t arlindhoxha/spring-petclinic:latest .
 docker login --username ${DOCKER_HUB_USERNAME} --password ${DOCKER_HUB_TOKEN_PASSWORD}
 docker push arlindhoxha/spring-petclinic:latest
