bddda76e36999aeae28980cbbfded12c514a3f8e
Arlind HOXHA <ahoxha.01@gmail.com>
2020-03-11 15:22:07 +0100

    syntax

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,5 @@ chmod +x docker-compose
 sudo mv docker-compose /usr/local/bin
 
 docker build -t ${DOCKER_HUB_USERNAME}/spring-petclinic:latest .
-docker login --username ${DOCKER_HUB_USERNAME} --password-stdin ${DOCKER_HUB_TOKEN_PASSWORD}
+docker login --username ${DOCKER_HUB_USERNAME} --password ${DOCKER_HUB_TOKEN_PASSWORD}
 docker push arlindhoxha/spring-petclinic:latest
