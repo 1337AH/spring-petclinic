29bf7464d2352c25f0cf80fd17432aef3392fba1
Arlind HOXHA <ahoxha.01@gmail.com>
2020-03-11 12:46:06 +0100

    artifacting script modified

--- scripts/artifacting.sh
+++ scripts/artifacting.sh
@@ -3,14 +3,15 @@
 sudo apt-get update && apt-get install -y python-pip
 sudo pip install awscli --ignore-installed six
 
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
-export AWS_SECRET_ACCESS_KEY=$AWS_ACCESS_KEY_ID
+# export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+# export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+# export AWS_SECRET_ACCESS_KEY=$AWS_ACCESS_KEY_ID
 
-sudo aws s3 cp target/spring-petclinic-2.2.0.BUILD-SNAPSHOT.jar s3://elasticbeanstalk-petclinic/
 # [OPTIONAL]
 # ----------------------------------------------------
-# aws configure set aws_access_key_id AWS_ACCESS_KEY_ID
-# aws configure set aws_secret_access_key AWS_SECRET_ACCESS_KEY
-# aws configure set default.region AWS_DEFAULT_REGION
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+aws configure set default.region "$AWS_DEFAULT_REGION"
 # ----------------------------------------------------
+
+sudo aws s3 cp target/spring-petclinic-2.2.0.BUILD-SNAPSHOT.jar s3://elasticbeanstalk-petclinic/
